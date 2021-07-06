9112c463d779f71e4db4b378c62cf725fcfe3b1e
Noelia Ruiz Martínez <nrm1977@gmail.com>
2021-07-06 13:26:21 +0200

    Validate addonFiles

--- _validate/validate.py
+++ _validate/validate.py
@@ -9,32 +9,15 @@ import os
 import sys
 import tempfile
 import zipfile
-import json
 import urllib.request
-from jsonschema import validate, exceptions
 
 sys.path.append(os.path.dirname(__file__))
 import sha256
 from addonManifest import AddonManifest
 del sys.path[-1]
 
-
-JSON_SCHEMA = os.path.join(os.path.dirname(__file__), "addonVersion_schema.json")
 DOWNLOAD_BLOCK_SIZE = 8192 # 8 kb
 TEMP_DIR = tempfile.gettempdir()
-
-def getAddonMetadata(filename):
-	with open(filename) as f:
-		data = json.load(f)
-	return data
-
-def validateJson(data):
-	with open(JSON_SCHEMA) as f:
-		schema = json.load(f)
-	try:
-		validate(instance=data, schema=schema)
-	except exceptions.ValidationError as err:
-		raise err
 
 def getDownloadUrlErrors(url):
 	errors = []
@@ -44,7 +27,7 @@ def getDownloadUrlErrors(url):
 		errors.append("add-on download url must end with .nvda-addon")
 	return errors
 
-def _downloadAddon(url):
+def downloadAddon(url):
 	destPath = os.path.join(TEMP_DIR, "addon.nvda-addon")
 	remote = urllib.request.urlopen(url)
 	if remote.code != 200:
@@ -63,15 +46,11 @@ def _downloadAddon(url):
 			local.write(block)
 	return destPath
 
-def getSha256Errors(destPath, data):
-	errors = []
+def getSha256(destPath):
 	with open(destPath, "rb") as f:
 		sha256Addon = sha256.sha256_checksum(f)
-	if sha256Addon != data["sha256"]:
-		errors.append(f"sha256 must be set to {sha256Addon} in json file")
-	return errors
 
-def _getAddonManifest(destPath):
+def getAddonManifest(destPath):
 	expandedPath = os.path.join(TEMP_DIR, "nvda-addon")
 	with zipfile.ZipFile(destPath, "r") as z:
 		for info in z.infolist():
@@ -83,70 +62,16 @@ def _getAddonManifest(destPath):
 	except Exception as err:
 		raise err
 
-def getSummaryErrors(manifest, data):
-	errors = []
-	summary = manifest["summary"]
-	if summary != data["name"]:
-		errors.append(f"name must be set to {summary} in json file")
-	return errors
-
-def getDescriptionErrors(manifest, data):
-	errors = []
-	description = manifest["description"]
-	if description != data["description"]:
-		errors.append(f"description must be set to {description} in json file")
-	return errors
-
-def getUrlErrors(manifest, data):
-	errors = []
-	url = manifest["url"]
-	if url != data["homepage"]:
-		errors.append(f"homepage must be set to {url} in json file")
-	return errors
-
-def getNameErrors(manifest, filename):
-	errors = []
-	name = manifest["name"]
-	if name != os.path.basename(os.path.dirname(filename)):
-		errors.append(f"json file must be placed in {name} folder")
-	return errors
-
-def getVersionErrors(manifest, filename):
-	errors = []
-	version = manifest["version"]
-	if version != os.path.splitext(os.path.basename(filename))[0]:
-		errors.append(f"jsonfile should be named {version}.json")
-	return errors
-
-
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument(
 		dest="file",
-		help="The json (.json) file containing add-on metadata."
+		help="The add-on download URL."
 	)
 	args = parser.parse_args()
-	filename = args.file
-	data = getAddonMetadata(filename=filename)
-	validateJson(data=data)
-	errors = []
-	url = data["URL"]
-	errors.extend(getDownloadUrlErrors(url))
-	if len(errors) > 0:
-		print("\r\n".join(errors))
-		raise ValueError("URL is not valid")
-	destPath = _downloadAddon(url=url)
+	url = args.file
+	destPath = downloadAddon(url=url)
 	manifest = _getAddonManifest(destPath=destPath)
-	errors.extend(getSha256Errors(destPath=destPath, data=data))
-	errors.extend(getSummaryErrors(manifest=manifest, data=data))
-	errors.extend(getDescriptionErrors(manifest=manifest, data=data))
-	errors.extend(getUrlErrors(manifest=manifest, data=data))
-	errors.extend(getNameErrors(manifest=manifest, filename=filename))
-	errors.extend(getVersionErrors(manifest=manifest, filename=filename))
-	if len(errors) > 0:
-		print("\r\n".join(errors))
-		raise ValueError("Json file is not valid")
-	print("Congratulations: manifest, metadata and file path are valid")
 
 if __name__ == '__main__':
 	main()
