18cc72cea5e9d165e8b21878cb24e0e450714419
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-11-08 11:35:30 +0100

    Ignore unknown type

--- _validate/createJson.py
+++ _validate/createJson.py
@@ -176,7 +176,7 @@ def _createDataclassMatchingJsonSchema(
 	return addonData
 
 
-def stripEmptyChangelog(translations: list[dict[str, str | None]]) -> list[dict[str, str]]:
+def stripEmptyChangelog(translations: list[dict[str, str | None]]) -> list[dict[str, str]]:  # type: ignore[reportUnknownMemberType]
 	"""Remove None changelogs from a list of translations.
 	return: A list of translations without None changelogs.
 	"""
